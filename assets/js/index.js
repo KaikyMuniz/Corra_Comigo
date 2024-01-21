ee24c546291ee93eedfd52dc826412d6eaa83705
Kaiky Muniz <130181837+KaikyMuniz@users.noreply.github.com>
2024-01-20 23:54:08 -0300

    Create index.js

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+const btnRedirecionar = document.querySelector(#btnIntroducao);
+
+btnRedirecionar.addEventListener('click', Redirecionar);
+
+function Redirecionar(e) {
+  window.alert(Abrir nova tela);
+}
